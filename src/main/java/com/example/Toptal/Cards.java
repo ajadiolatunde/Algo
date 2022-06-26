8a2aa0d47dfe1bf25836dd39ee882b1b7bc27007
ajadiolatunde <ajadiolatunde@gmail.com>
2022-06-26 16:51:15 +0100

    first commit

---
+++ src/main/java/com/example/Toptal/Cards.java
@@ -0,0 +1,32 @@
+package com.example.Toptal;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cards {
+    private static int getSmallest(int[] a,int[] b){
+        int[] loader = new int[a.length];
+        for (int x=0;x<b.length;x++){
+            int[] new_a = a.clone();
+            new_a[x] =b[x];
+            Arrays.sort(new_a);
+            int min = new_a[0];
+            int max = new_a[b.length-1];
+            for (int y=min;x<max;y++){
+                int finalY = y;
+                boolean contain = Arrays.stream(new_a).anyMatch(e->e== finalY);
+                if (!contain){
+                    loader[x] = finalY;
+                    break;
+                }
+            }
+        }
+        Arrays.sort(loader);
+        return loader[0];
+    }
+    public static void main(String[] args){
+        System.out.println(getSmallest(new int[]{1,2},new int[]{1,2}));
+
+    }
+}
