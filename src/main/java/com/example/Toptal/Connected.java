ec501927506cc2b9c8498d9e15a6ac5b3aadd97b
ajadiolatunde <ajadiolatunde@gmail.com>
2022-06-28 11:23:22 +0100

    Second commit

--- src/main/java/com/example/Toptal/Connected.java
+++ src/main/java/com/example/Toptal/Connected.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Connected {
+    private static int ans = 0;
     public static class edge{
         int city1;
         int city2;
@@ -19,43 +20,99 @@ public class Connected {
         }
     }
 
-    private static int getConnectedComponent(Map<Integer, List<Integer>> adjs,int count){
+    private static void getSetConnectedComponent(Map<Integer, Set<Integer>> adjs,int count){
         List<Integer> keys =  adjs.keySet().stream().collect(Collectors.toList());
-
+        boolean stop = true;
         for (int x=0;x<keys.size();x++){
+            if (!stop){
+                break;
+            }
             Iterator<Integer> ik = adjs.keySet().iterator();
             while(ik.hasNext()){
                 int ike = ik.next();
-
                 if (adjs.get(ike).contains(keys.get(x))){
-                    System.out.println("-----Stage2------");
-                    //get the list of current key and merge with that list
-                    //get the content out
-                    List<Integer> due_remove = adjs.get(keys.get(x));
+                    Set<Integer> due_remove = adjs.get(keys.get(x));
                     //Merge the
-                    List<Integer> adlist =  adjs.get(ike);
-                    adlist.remove(adlist.indexOf(keys.get(x)));
-                    adlist.addAll(due_remove);
+                    adjs.get(ike).remove( keys.get(x));
+                    adjs.get(ike).addAll(due_remove);
+                    adjs.get(ike).remove(ike);
                     adjs.remove(keys.get(x));
-                    adjs.put(ike,adlist);
+
+                    adjs.put(ike, adjs.get(ike));
                     count++;
-                    getConnectedComponent(adjs,count);
-                    System.out.println("-----------");
+                    System.out.println(adjs.entrySet());
 
-                    System.out.println(adjs);
+                    getSetConnectedComponent(adjs,count);
+                    stop = false;
                     break;
-
                 }
-
             }
         }
+        //System.out.println(adjs.size());
+        //System.out.println(adjs.entrySet());
 
-        System.out.println(adjs.size());
+        ans=  adjs.size();
+    }
+
+    private static int getConnectedComponent(Map<Integer, List<Integer>> adjs,int count){
+        List<Integer> keys =  adjs.keySet().stream().collect(Collectors.toList());
+        boolean stop = false;
+        for (int x=0;x<keys.size();x++){
+            if (stop){
+                break;
+            }
+            Iterator<Integer> ik = adjs.keySet().iterator();
+            while(ik.hasNext()){
+                int ike = ik.next();
+                if (adjs.get(ike).contains(keys.get(x))){
+                    List<Integer> due_remove = adjs.get(keys.get(x));
+                    //Merge the
+                    adjs.get(ike).remove( adjs.get(ike).indexOf(keys.get(x)));
+                    adjs.get(ike).addAll(due_remove);
+                    adjs.remove(keys.get(x));
+                    adjs.put(ike, adjs.get(ike));
+                    count++;
+                    getConnectedComponent(adjs,count);
+                    stop = true;
+                    break;
+                }
+            }
+        }
+        //System.out.println(adjs.size());
+        //System.out.println(adjs.entrySet());
         return adjs.size();
     }
+
+    private static Map<Integer, Set<Integer>>  buildNodeAdjacencicy(List<edge> obj){
+        Map<Integer, Set<Integer>> adj =  new HashMap<>();
+        obj.stream().forEach(
+                x-> {
+                    Set<Integer> cit1 = new HashSet<>();
+                    cit1.add(x.city2);
+                    if (adj.containsKey(x.city1)){
+                        Set<Integer> nk = adj.get(x.city1);
+                        nk.add(x.city2);
+                        adj.put(x.city1,nk);
+                    }else{
+                        adj.put(x.city1,cit1);
+                    }
+                    Set<Integer> cit2 = new HashSet<>();
+                    cit2.add(x.city1);
+                    if (adj.containsKey(x.city2)){
+                        Set<Integer> nk = adj.get(x.city2);
+                        nk.add(x.city1);
+                        adj.put(x.city2,nk);
+                    }else{
+                        adj.put(x.city2,cit2);
+                    }
+
+                }
+        );
+        System.out.println(adj.entrySet());
+      return adj;
+    }
+
     private static Map<Integer, List<Integer>>  buildAdjacencicy(List<edge> obj){
-        //First do flat
-        //Map<String, Set<Integer>> adj = new HashMap<String,Set<Integer>>();
         Map<Integer, List<Integer>> adj = obj.stream()
                 .collect(Collectors.groupingBy(x->x.city1,Collectors.mapping(x->x.city2, Collectors.toList())))
                .entrySet().stream()
@@ -63,21 +120,17 @@ public class Connected {
         System.out.println(adj);
         //After sort
         Comparator<Map.Entry<Integer, List<Integer>>> sorted = Comparator.comparingInt(stringListEntry -> stringListEntry.getValue().get(0));
-        adj.entrySet().stream().sorted(sorted).forEach(System.out::println);
-        //System.out.println(adj);
-
         return adj;
     }
-
     public static void main(String[] args){
         List<edge> array = new ArrayList<>(Arrays.asList(new edge(8,5),new edge(1,2),new edge(2,3),new edge(2,4),new edge(3,5),new edge(6,7)));
-       // getConnectedComponent(array);
-        Map<Integer, List<Integer>> adj =buildAdjacencicy(array);
-        getConnectedComponent(adj,0);
-
-
+        List<edge> array1 = new ArrayList<>(Arrays.asList(new edge(0,1),new edge(1,2),new edge(3,4)));
+        Map<Integer, List<Integer>> adj =buildAdjacencicy(array1);
+       // getConnectedComponent(adj,0);
+        Map<Integer, Set<Integer>> adjs = buildNodeAdjacencicy(array1);
+        getSetConnectedComponent(adjs,0);
+        System.out.println("--ans--"+ans);
 
     }
-
 
 }
--- src/main/java/com/example/Toptal/SortNumber.java
+++ src/main/java/com/example/Toptal/SortNumber.java
@@ -1,4 +1,29 @@
 package com.example.Toptal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SortNumber {
+
+    public static void main(String[] args){
+        int[] list =new int[]{1,2,5,8,2};
+        for (int i=0; i<list.length; i++){
+            for (int j=0; j<list.length; j++){
+                if (list[i]<list[j]){
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        List<String> kl = Arrays.stream(list)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toList());
+
+        System.out.println(kl);
+
+
+    }
 }
