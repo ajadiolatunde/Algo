44be94d9c99f09df4e9b67fce28b02d5259a8324
ajadiolatunde <ajadiolatunde@gmail.com>
2022-06-29 14:35:08 +0100

    Second commit

--- src/main/java/com/example/Toptal/Connected.java
+++ src/main/java/com/example/Toptal/Connected.java
@@ -5,6 +5,12 @@ import java.util.stream.Collectors;
 
 /***
  * Get numbers of connected component in undirected graph
+ * Step 1:Get list of all node components
+ * Step 2:Build adjacency list with node components
+ * Step 3:Conduct a union test by
+ *      - Look for parent relationship by collapsing a related link
+ *      - If a parent is found in another parent neighbour list then merge the parent neirbour list and remove the parent node from the list .
+ *      -Perform forward and backward iteration by comparing key to all available keyset values
  */
 
 public class Connected {
@@ -24,7 +30,7 @@ public class Connected {
         }
     }
 
-    private static void getSetConnectedComponent(Map<Integer, Set<Integer>> adjs,int count){
+    private static void getRecursiveSetConnectedComponent(Map<Integer, Set<Integer>> adjs,int count){
         List<Integer> keys =  adjs.keySet().stream().collect(Collectors.toList());
         boolean stop = true;
         for (int x=0;x<keys.size();x++){
@@ -46,7 +52,7 @@ public class Connected {
                     count++;
                     System.out.println(adjs.entrySet());
 
-                    getSetConnectedComponent(adjs,count);
+                    getRecursiveSetConnectedComponent(adjs,count);
                     stop = false;
                     break;
                 }
@@ -56,35 +62,49 @@ public class Connected {
         ans=  adjs.size();
     }
 
-    private static int getConnectedComponent(Map<Integer, List<Integer>> adjs,int count){
-        List<Integer> keys =  adjs.keySet().stream().collect(Collectors.toList());
+    private static int getConnectedComponent(Map<Integer, Set<Integer>> adjs){
+        int x = 0;
         boolean stop = false;
-        for (int x=0;x<keys.size();x++){
-            if (stop){
-                break;
-            }
-            Iterator<Integer> ik = adjs.keySet().iterator();
-            while(ik.hasNext()){
-                int ike = ik.next();
-                if (adjs.get(ike).contains(keys.get(x))){
-                    List<Integer> due_remove = adjs.get(keys.get(x));
-                    //Merge the
-                    adjs.get(ike).remove( adjs.get(ike).indexOf(keys.get(x)));
-                    adjs.get(ike).addAll(due_remove);
-                    adjs.remove(keys.get(x));
-                    adjs.put(ike, adjs.get(ike));
-                    count++;
-                    getConnectedComponent(adjs,count);
+        while (x<adjs.entrySet().size()-1){
+            stop = false;
+            List<Integer> keys =  adjs.keySet().stream().collect(Collectors.toList());
+            //Forward
+            for (int y=0;y<keys.size();y++){
+                //Find first key in first key set
+                if (adjs.get(keys.get(y)).contains(keys.get(x))){
+                    Set<Integer> set_list = adjs.get(keys.get(x));
+                    set_list.addAll(adjs.get(keys.get(y)));
+                    set_list.remove(keys.get(x));
+                    set_list.remove(keys.get(y));
+                    adjs.remove( keys.get(x));
+                    adjs.put(keys.get(y),set_list );
+                    stop = true;
+                    break;
+                }
+                //Reverse find last key in the first key set
+                if (adjs.get(keys.get(y)).contains(keys.get(adjs.entrySet().size()-1-x))){
+                    Set<Integer> set_list = adjs.get(keys.get(adjs.entrySet().size()-1-x));
+                    set_list.addAll(adjs.get(keys.get(y)));
+                    set_list.remove(keys.get(adjs.entrySet().size()-1-x));
+                    set_list.remove(keys.get(y));
+                    adjs.remove(keys.get(adjs.entrySet().size()-1-x));
+                    adjs.put(keys.get(y),set_list );
                     stop = true;
                     break;
                 }
             }
+            if (stop){
+                x=0;
+            }else{
+                x++;
+            }
+
         }
         //System.out.println(adjs.size());
         System.out.println(adjs.entrySet());
+        System.out.println(adjs.entrySet().size());
         return adjs.size();
     }
-
     private static Map<Integer, Set<Integer>>  buildNodeAdjacencicy(List<edge> obj){
         Map<Integer, Set<Integer>> adj =  new HashMap<>();
         obj.stream().forEach(
@@ -107,20 +127,20 @@ public class Connected {
                     }else{
                         adj.put(x.city2,cit2);
                     }
-
                 }
         );
         System.out.println(adj.entrySet());
       return adj;
     }
-
-
     public static void main(String[] args){
+        //Test data
         List<edge> array = new ArrayList<>(Arrays.asList(new edge(8,5),new edge(1,2),new edge(2,3),new edge(2,4),new edge(3,5),new edge(6,7)));// return 2
         List<edge> array1 = new ArrayList<>(Arrays.asList(new edge(0,1),new edge(1,2),new edge(2,3),new edge(3,4)));//return 1
-        Map<Integer, Set<Integer>> adjs = buildNodeAdjacencicy(array1);
-        getSetConnectedComponent(adjs,0);
-        System.out.println("--ans--"+ans);
+        List<edge> array2 = new ArrayList<>(Arrays.asList(new edge(1,2),new edge(2,4),new edge(5,7),new edge(7,2),new edge(9,5)));//return 1
+
+        Map<Integer, Set<Integer>> adjs = buildNodeAdjacencicy(array2);
+        //getSetConnectedComponent(adjs);
+        getConnectedComponent(adjs);
 
     }
 
