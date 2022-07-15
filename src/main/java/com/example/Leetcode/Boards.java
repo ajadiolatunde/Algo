d5553bd17bed3250d7d60e10f0cd160b273d2c01
ajadiolatunde <ajadiolatunde@gmail.com>
2022-07-15 15:44:34 +0100

    Word search class commit

--- src/main/java/com/example/Leetcode/Boards.java
+++ src/main/java/com/example/Leetcode/Boards.java
@@ -1,102 +1,131 @@
 package com.example.Leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Boards {
-    private static boolean exist(char[][] board,String words,String start){
-        //if (word.length()<1) return false;
-        String word = words;
-        String[] newstart = start.split("");
-        int r = Integer.parseInt(newstart[0]);
-        int c = Integer.parseInt(newstart[1]);
+    private static boolean exist(char[][] board,String words){
+        String word = words.substring(0,words.length());
+
+
         int row_len = board.length;
         int col_len = board[0].length;
-        boolean res = false;
-        Stack<String> tJunctions = new Stack<String>();
-        List<String>   word_coordList = new ArrayList<String>();
+        Stack<Map<String,String>> tJunctions = new Stack<>();
+        List<String>   word_coordList = new ArrayList<>();
 
+        for (int row=0;row<board.length;row++ ){
+            for (int col=0;col<board[0].length;col++ ){
+                //Try right
+                System.out.println(word);
+                if (word.length()<=1){
+                    word_coordList.add(row+""+col);
+                    word = "";
+                    break;
+                }
+                char current = word.charAt(0);
 
-
-        for (int row=r;row<board.length;row++ ){
-            for (int col=c;col<board[0].length;col++ ){
-                    //Try right
-                      char current = word.charAt(0);
-                    if (current== board[row][col]) {
+                if (current== board[row][col]) {
                         //Then check for tjunctions
                         int count_tjunctions =0;
                         boolean down = false,up = false,back = false,forward = false;
                         //for down
-                        if (row+1<row_len && !word_coordList.contains((r+1)+""+c) && word.charAt(1) == board[row+1][col] ){
+                        if (row+1<row_len && !word_coordList.contains((row+1)+""+col) && word.charAt(1) == board[row+1][col] ){
                             count_tjunctions++;
                             down = true;
                         }
                         //for up
-                        if (row>0 && !word_coordList.contains((r-1)+""+c) && word.charAt(1) == board[row-1][col] ){
+                        if (row>0 && !word_coordList.contains((row-1)+""+col) && word.charAt(1) == board[row-1][col] ){
                             up = true;
                             count_tjunctions++;
                         }
                         //Forward or column
-                        if (col+1<col_len && !word_coordList.contains((r)+""+c+1) && word.charAt(1) == board[row][col+1] ){
+                        if (col+1<col_len && !word_coordList.contains(row+""+(col+1)) && word.charAt(1) == board[row][col+1] ){
                             forward = true;
                             count_tjunctions++;
                         }
                         //backward
-                        if (col>0 && !word_coordList.contains((r)+""+c+1) && word.charAt(1) == board[row][col-1] ){
+                        if (col>0 && !word_coordList.contains(row+""+(col+1)) && word.charAt(1) == board[row][col-1] ){
                             back = true;
                             count_tjunctions++;
                         }
                         if (count_tjunctions>1){
-                             if (up)tJunctions.add((row-1)+""+col);
-                            if (down)tJunctions.add((row+1)+""+col);
-                            if (forward)tJunctions.add(row+""+col);
-                            if (back)tJunctions.add(row+""+(col-2));
+                            word =word.replaceFirst(String.valueOf(current),"");
+                            //word = str.substring(1, str.length() - 1);
+                             if (up){
+                                 Map<String,String> mup = new HashMap<>();
+                                 mup.put(word,(row-1)+""+col);
+                                 tJunctions.add(mup);
+                             }
+                            if (down){
+                                Map<String,String> mdown = new HashMap<>();
+                                mdown.put(word,row+1+""+col);
+                                tJunctions.add(mdown);
+                            }
+                            if (forward){
+                                Map<String,String> mforward = new HashMap<>();
+                                mforward.put(word,row+""+col);
+                                tJunctions.add(mforward);
+                            }
+                            if (back){
+                                Map<String,String> mback = new HashMap<>();
+                                mback.put(word,row+""+(col-2));
+                                tJunctions.add(mback);
+                            }
                             //Add current
-                            word_coordList.add(r+""+c);
+                            word_coordList.add(row+""+col);
                             //Remove found char from word
-                            word.replaceFirst(String.valueOf(current),"");
                             //Get next position fron stack;
-                            String[] rc = tJunctions.pop().split("");
-                            r = Integer.parseInt(rc[0]);
-                            c= Integer.parseInt(rc[1]);
-
-
+                            Map<String,String> mnext = tJunctions.pop();
+                            String[] rc = mnext.values().iterator().next().split("");
+                            row = Integer.parseInt(rc[0]);
+                            col= Integer.parseInt(rc[1]);
                             //No tjunctions
                         }else if (count_tjunctions == 1){
                             //Add current and move to next coord position
-                            word_coordList.add(r+""+c);
+                            //and readjust word_coordList to suit current position
+                            word_coordList.add(row+""+col);
+                            word = word.replaceFirst(String.valueOf(current),"");
+
+
                             if (up){
                                 row -= 1;
                             }else if (down){
                                 row +=1;
+                                col -= 1;
                             }else if (forward){
                                col = col;
                             }
                             else{
                                 col -= 2;
                             }
-
                         }else{
                             // Nothing found
                             // Move to next tjunction
+                            if (tJunctions.size()>0) {
+                                Map<String, String> mnext = tJunctions.pop();
+                                String coord = mnext.values().iterator().next();
+                                String[] rc = coord.split("");
+                                row = Integer.parseInt(rc[0]);
+                                col = Integer.parseInt(rc[1]);
+                                //Restore  word to point of this tjunction
+                                word = mnext.keySet().iterator().next();
+                                word_coordList = word_coordList.subList(0,word_coordList.indexOf(coord)+1);
+                            }else{
+                                // Else move to defaut loop and start all over
+                                word_coordList.clear();
+                                word = words;
+                            }
                         }
-
                     }
-
-
-
             }
         }
-        return true;
+        System.out.println(word);
+        return word.isEmpty();
     }
 
     public static void main(String[] args){
         char[][] ev  = {{'a','b'},{'c','d'}};
-        String v = "aaabaacd";
-        String w = v.replaceFirst("a","");
-        System.out.println(w);
-        //System.out.println(exist(ev,v,"00"));
-
+        String v = "abd";
+        char c = 'a';
+       System.out.println(exist(ev,v));
     }
 }
